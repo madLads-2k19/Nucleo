092345a16ef51de1cdbb0ff3ec495743559afc4e
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2020-12-22 01:56:10 +0530

    Switching to Postgres

--- src/dependencies/database/database.py
+++ src/dependencies/database/database.py
@@ -1,50 +1,35 @@
 import asyncio
 
-import aiomysql
+import asyncpg
 
-# from . import database_exceptions
-from . import *
+from .database_exceptions import *
 
 
-class Database:
+class PgDatabase:
     def __init__(self, database_host: str, database_name: str, database_user: str, database_password,
-                 database_port: int = 3306, min_conns: int = 3, max_conns: int = 10,
+                 database_port: int = 5432, min_conns: int = 3, max_conns: int = 10,
                  loop: asyncio.AbstractEventLoop = None):
-        self.tasks = []
         self.db_connections = {}
-        self.db_pool: aiomysql.Pool
+        self.db_pool: asyncpg.pool.Pool
         self.running = False
-        self.database_data = {'minsize': min_conns, 'maxsize': max_conns, 'host': database_host, 'user': database_user,
-                              'password': database_password, 'db': database_name, 'port': database_port,
-                              'autocommit': True}
-        if loop is None:
-            self.loop = asyncio.get_event_loop()
-        else:
-            self.loop = loop
-        self.__async_init_task = self.loop.create_task(self.__pool_starter__())
+        self._database_data = {'dsn': f'postgres://{database_user}:{database_password}'
+                                      f'@{database_host}:{database_port}/{database_name}', 'min_size': min_conns,
+                               'max_size': max_conns}
 
-    async def __task_handler__(self):
-        while self.running:
-            try:
-                if len(self.tasks) > 0:
-                    task: asyncio.Task = self.tasks.pop()
-                    await task
-                else:
-                    await asyncio.sleep(1)
-            except Exception as e:
-                # TODO: log this somehow
-                pass
-        return
+        self.loop = loop
+        if self.loop is None:
+            self.loop = asyncio.get_event_loop()
+
+        self.__async_init_task = self.loop.create_task(self.__pool_starter__())
 
     async def __pool_starter__(self):
         try:
-            self.db_pool = await aiomysql.create_pool(**self.database_data)
+            self.db_pool: asyncpg.pool.Pool = await asyncpg.create_pool(**self._database_data)
             self.running = True
-            self.loop.create_task(self.__task_handler__())
-            print('Database successfully connected')
+            await self.__database_initializer__()
             return True
         except Exception as e:
-            raise database_exceptions.DatabaseInitError(f'Databased failed to start with error:  {e}, type:  {type(e)}')
+            raise DatabaseInitError(f'Databased failed to start with error:  {e}, type:  {type(e)}')
 
     async def __init_check__(self):
         if self.running:
@@ -53,12 +38,10 @@ class Database:
             await self.__async_init_task
             return
 
-    async def __cursor_creator__(self):
+    async def test(self):
         await self.__init_check__()
-        connection: aiomysql.Connection = await self.db_pool.acquire()
-        cursor: aiomysql.Cursor = await connection.cursor()
-        self.db_connections[cursor] = connection
-        return cursor
+        data = await self.db_pool.fetch('SELECT version();')
+        print(data)
 
     def __cursor_recycle__(self, cursor: aiomysql.Cursor):
         connection: aiomysql.Connection = self.db_connections[cursor]
@@ -68,89 +51,50 @@ class Database:
 
     async def permission_retriever(self, *ids, with_name=False):
         if len(ids) == 0:
-            raise database_exceptions.DatabaseMissingArguments(f'Missing arguments at the permission retriever')
-        cursor = await self.__cursor_creator__()
+            raise DatabaseMissingArguments(f'Missing arguments at the permission retriever')
         if with_name:
-            query = f'SELECT MAX(USER_AUTH.`LEVEL`), NAME FROM USER_AUTH, PERMISSIONS_NAMES WHERE ITEM_ID IN ' \
-                    f'({", ".join(["%s"] * len(ids))}) AND USER_AUTH.`LEVEL` = PERMISSIONS_NAMES.`LEVEL`'
+            query = f'SELECT MAX(LEVEL), NAME FROM USER_AUTH INNER JOIN PERMISSIONS_NAMES USING (LEVEL) ' \
+                    f'WHERE ITEM_ID IN({", ".join(f"${x + 1}" for x in range(len(ids)))})'
         else:
-            query = f'SELECT MAX(LEVEL) FROM USER_AUTH WHERE ITEM_ID IN ({", ".join(["%s"] * len(ids))})'
-        await cursor.execute(query, ids)
-        data = await cursor.fetchone()
-        permission = data[0]
-        self.__cursor_recycle__(cursor)
+            query = f'SELECT MAX(LEVEL) FROM USER_AUTH WHERE ITEM_ID IN ({", ".join(f"${x + 1}" for x in range(len(ids)))}) '
+        data = await self.db_pool.fetchrow(query, *ids)
+        permission_level = data[0]
         if with_name:
-            return permission, data[1]
+            return permission_level, data[1]
         else:
-            return permission
+            return permission_level
 
     async def auth_retriever(self, include_roles: bool = False):
-        cursor = await self.__cursor_creator__()
-        query = 'SELECT USER_AUTH.`ITEM_ID`, USER_AUTH.`LEVEL`, PERMISSIONS_NAMES.`Name`, USER_AUTH.`ROLE` ' \
-                'FROM USER_AUTH, PERMISSIONS_NAMES WHERE USER_AUTH.`LEVEL` = PERMISSIONS_NAMES.`LEVEL`'
+        query = 'SELECT ITEM_ID, LEVEL, Name, ROLE FROM USER_AUTH INNER JOIN PERMISSIONS_NAMES USING (LEVEL)'
         if include_roles is False:
             query = ' '.join((query, 'AND USER_AUTH.`ROLE` = 0'))
-        await cursor.execute(query)
-        data = await cursor.fetchall()
-        self.__cursor_recycle__(cursor)
-        return ({'id': item[0], 'level': item[1], 'nick': item[2], 'role': bool(item[3])} for item in data)
+
+        data = await self.db_pool.fetch(query)
+        return [{'id': item[0], 'level': item[1], 'nick': item[2], 'role': bool(item[3])} for item in data]
 
     async def auth_adder(self, target_id: int, level: int, role: bool = False, server_id: int = 0):
-        query = 'INSERT INTO USER_AUTH (ITEM_ID, LEVEL, ROLE, SERVER_ID) VALUES (%s, %s, %s, %s)'
-        cursor = await self.__cursor_creator__()
+        query = 'INSERT INTO USER_AUTH (ITEM_ID, LEVEL, ROLE, SERVER_ID) VALUES ($1, $2, $3, $4)'
         try:
-            await cursor.execute(query, (target_id, level, int(role), server_id))
-        except aiomysql.IntegrityError:
+            await self.db_pool.execute(query, target_id, level, int(role), server_id)
+        except asyncpg.IntegrityConstraintViolationError:
             raise DatabaseDuplicateEntry
-        except Exception as e:
-            # print(e, type(e))
-            raise e
-        finally:
-            self.__cursor_recycle__(cursor)
 
     async def auth_changer(self, target_id: int, level: int):
-        query = 'UPDATE USER_AUTH set LEVEL = %s where ITEM_ID = %s'
-        cursor = await self.__cursor_creator__()
-        try:
-            await cursor.execute(query, (level, target_id))
-        except Exception as e:
-            raise e
-        finally:
-            self.__cursor_recycle__(cursor)
+        query = 'UPDATE USER_AUTH set LEVEL = $1 where ITEM_ID = $2'
+        await self.db_pool.execute(query, level, target_id)
 
-    async def whitelist_check(self, server_id: int, channel_id: int):
-        query = 'SELECT count(1) FROM CHANNEL_AUTH WHERE SERVER_ID = %s AND CHANNEL_ID = %s'
-        cursor = await self.__cursor_creator__()
-        try:
-            await cursor.execute(query, (server_id, channel_id))
-            data = await cursor.fetchone()
-            result = bool(data[0])
-        except Exception as e:
-            raise e
-        finally:
-            self.__cursor_recycle__(cursor)
-        return result
+    async def whitelist_check(self, server_id: int, channel_id: int) -> int:
+        query = 'SELECT WHITELIST_LEVEL FROM CHANNEL_AUTH WHERE SERVER_ID = $1 AND CHANNEL_ID = $2'
+        data = await self.db_pool.fetchval(query, server_id, channel_id)
+        return data
 
-    async def whitelist_add(self, server_id: int, channel_id: int):
-        query = 'INSERT INTO CHANNEL_AUTH (SERVER_ID, CHANNEL_ID) VALUES (%s, %s)'
-        cursor = await self.__cursor_creator__()
+    async def whitelist_add(self, server_id: int, channel_id: int, whitelist_level: int = 1):
+        query = 'INSERT INTO CHANNEL_AUTH (SERVER_ID, CHANNEL_ID, WHITELIST_LEVEL) VALUES ($1, $2, $3)'
         try:
-            await cursor.execute(query, (server_id, channel_id))
-        except aiomysql.IntegrityError:
+            await self.db_pool.execute(query, server_id, channel_id, whitelist_level)
+        except asyncpg.IntegrityConstraintViolationError:
             raise DatabaseDuplicateEntry('Duplicate values at CHANNEL AUTH')
-        except Exception as e:
-            raise e
-        finally:
-            self.__cursor_recycle__(cursor)
-        return
 
     async def whitelist_remove(self, server_id: int, channel_id: int):
-        query = 'DELETE FROM CHANNEL_AUTH WHERE SERVER_ID = %s AND CHANNEL_ID = %s'
-        cursor = await self.__cursor_creator__()
-        try:
-            await cursor.execute(query, (server_id, channel_id))
-        except Exception as e:
-            raise e
-        finally:
-            self.__cursor_recycle__(cursor)
-        return
+        query = 'DELETE FROM CHANNEL_AUTH WHERE SERVER_ID = $1 AND CHANNEL_ID = $2'
+        await self.db_pool.execute(query, server_id, channel_id)
