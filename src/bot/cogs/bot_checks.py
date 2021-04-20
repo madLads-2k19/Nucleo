f681335c9cebea8c371f5de73ab733c77ce084c7
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-04-20 12:40:40 +0530

    Bug fix in permission level checking

--- src/bot/cogs/bot_checks.py
+++ src/bot/cogs/bot_checks.py
@@ -16,8 +16,8 @@ def check_permission_level(required_level: int = 0):
         perm = await db.permission_retriever(*ids)
         if perm is None:
             perm = 0
-        if perm >= required_level or is_owner:
-            return True
+        if perm <= required_level or is_owner:
+            return perm
         raise bot_exceptions.NotEnoughPerms(f"{ctx.author} does not have enough permission to run the command")
 
     return commands.check(check)
