fc1cd98fc548efd27bfd16b56bcad7fe95cd1589
RazCrimson <52282402+RazCrimson@users.noreply.github.com>
2021-07-18 21:40:53 +0530

    Fix for CheckFailures

--- src/bot/cogs/bot_checks.py
+++ src/bot/cogs/bot_checks.py
@@ -11,17 +11,19 @@ def check_permission_level(required_level: int = 0):
         bot = ctx.bot
         db: Database = bot.db
         author: discord.Member = ctx.author
-        is_owner: bool = await ctx.bot.is_owner(ctx.author)
-        ids = [author.id, *[role.id for role in author.roles]]
+        is_god: bool = await ctx.bot.is_owner(ctx.author)
+        if hasattr(author, "roles"):
+            ids = [author.id, *[role.id for role in author.roles]]
+        else:
+            ids = [author.id]
         perm = await db.permission_retriever(*ids)
         if perm is None:
             perm = 0
-        if perm >= required_level or is_owner:
-            return perm
+        if perm >= required_level or is_god:
+            return True
         raise bot_exceptions.NotEnoughPerms(f"{ctx.author} does not have enough permission to run the command")
 
     return commands.check(check)
-
 
 def is_whitelist():
     async def check(ctx: Context):
