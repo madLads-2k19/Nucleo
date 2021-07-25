980abff09b537462ad07153b123a76eebc65947d
RazCrimson <52282402+RazCrimson@users.noreply.github.com>
2021-07-25 21:33:28 +0530

    ModCommands: Add say and react command

--- src/bot/cogs/mod_commands.py
+++ src/bot/cogs/mod_commands.py
@@ -1,3 +1,5 @@
+from typing import Optional, Union
+
 import discord
 from discord.ext import commands
 from discord.ext.commands import Context
@@ -37,6 +39,49 @@ class ModCommands(commands.Cog):
             count = 500
         deleted = await ctx.channel.purge(limit=count + 1, check=user_check)
         await ctx.send('Deleted {} message(s)'.format(len(deleted)), delete_after=5)
+
+    @commands.command(brief='Send the message content to the corresponding channel')
+    @bot_checks.check_permission_level(8)
+    async def say(self, ctx: Context, channel_id: int):
+        if channel_id:
+            try:
+                channel: discord.TextChannel = self.bot.get_channel(channel_id)
+            except discord.NotFound:
+                return await ctx.reply("Invalid Channel ID")
+        else:
+            channel = ctx.channel
+        m = await ctx.reply('Please send the message....')
+
+        def author_check(message):
+            if message.channel.id == ctx.channel.id and message.author == ctx.author:
+                return True
+
+        try:
+            msg = await self.bot.wait_for('message', check=author_check, timeout=30)
+            await channel.send(msg.content)
+            await ctx.reply("Message sent")
+        except TimeoutError:
+            await ctx.reply("Timed out!")
+        await m.delete()
+
+    @commands.command(brief='Reacts to a message')
+    @bot_checks.check_permission_level(8)
+    async def react(self, ctx: Context, emoji: Union[discord.PartialEmoji, str], message_id: int, channel_id: Optional[int]):
+        if channel_id:
+            try:
+                channel: discord.TextChannel = self.bot.get_channel(channel_id)
+            except discord.NotFound:
+                return await ctx.reply("Invalid Channel ID")
+        else:
+            channel = ctx.channel
+
+        try:
+            msg = await channel.fetch_message(message_id)
+            await msg.add_reaction(emoji)
+        except discord.NotFound:
+            await ctx.reply(f"Cannot find message with ID: `{message_id}`")
+        except discord.Forbidden:
+            await ctx.reply(f"I dont have enough Permissions!")
 
 
 def setup(bot):
